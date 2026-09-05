3bd7e04eb27f6bdc2e576835260d83118ee583ca
andrew99 <earlbosworth159@gmail.com>
2026-09-05 21:56:19 +0700

    flash: bump SSID to GTIControl1253

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@
   #define DBG_PRINTLN(...) do {} while (0)
 #endif
 
-#define WIFI_BROADCAST_SSID "GTIControl1252"
+#define WIFI_BROADCAST_SSID "GTIControl1253"
 
 #define KEY_SPLIT "&&&&"
 #define KEY_SPLIT_DATA "#"
